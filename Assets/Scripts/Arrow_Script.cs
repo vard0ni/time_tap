e78ec41aabcea8a3a80267b4a8111016e632cbed
Zakhar Gapeev <ip_goose@mail.ru>
2020-10-18 20:41:41 +0700

    init

---
+++ Assets/Scripts/Arrow_Script.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow_Script : MonoBehaviour
+{
+	public Action<float> Clicked;
+	public AnimationCurve curve;
+
+	public Vector3 positionA;  // left
+	public Vector3 positionB;  // right
+
+	public Quaternion rotationA;
+	public Quaternion rotationB;
+
+	[Range(0.1f, 3.0f)]
+	public float speed = 2.0f;
+
+	float t = 0;
+	bool goingForward;
+
+	void Update()
+	{
+		if (goingForward)
+			t += Time.deltaTime * speed;
+		else
+			t -= Time.deltaTime * speed;
+
+		if (t < 0.0f || t > 1.0f)
+			goingForward = !goingForward;
+		t = Mathf.Clamp01(t);
+		// float t = Mathf.Sin(Time.time * speed) * 0.5f + 0.5f; // 0.0f...1.0f
+
+		//transform.position = Vector3.Lerp(positionA, positionB, curve.Evaluate(t));
+		transform.rotation = Quaternion.Lerp(rotationA, rotationB, curve.Evaluate(t));
+	}
+	public void OnClick()
+	{
+		var error = Mathf.Abs(curve.Evaluate(t) - 0.5f);
+		Clicked?.Invoke(error);
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using Debug = UnityEngine.Debug;
+
+public class GameManager : MonoBehaviour
+{
+	public Arrow_Script arrow;
+
+	void OnValidate()
+	{
+		if (arrow == null)
+			arrow = FindObjectOfType<Arrow_Script>();
+	}
+
+	// Update is called once per frame
+	void onEnable()
+	{
+		arrow.Clicked += OnClicked;
+	}
+	void onDisable()
+	{
+		arrow.Clicked -= OnClicked;
+	}
+	void OnClicked(float t)
+	{
+		t = 1.0f - 2.0f * t;
+		arrow.enabled = false;
+		Debug.Log($"{t:F5}");
+	}
+}
